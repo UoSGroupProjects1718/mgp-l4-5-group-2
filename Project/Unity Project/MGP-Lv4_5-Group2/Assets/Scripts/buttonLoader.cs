473e464c241971273660b2be05fa9abad76f4e8f
Daniel Beales <s185384@ucs.ac.uk>
2018-02-05 14:33:31 +0000

    unity Project Added and built.
    Built,
    Menu system,
    Basic scripts,
    few assets.

---
+++ Project/Unity Project/MGP-Lv4_5-Group2/Assets/Scripts/buttonLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class buttonLoader : MonoBehaviour {
+
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    public void ExitGame(bool bool_Quit)
+    {
+        Application.Quit();
+    }
+
+}
